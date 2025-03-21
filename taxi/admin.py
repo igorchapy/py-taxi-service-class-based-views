5616013598dfe9a5d3b15eca215f11c0ea1989bd
Igor Chapy <misha@mate.acedemy>
2025-03-21 16:18:11 +0200

    Solution

--- taxi/admin.py
+++ taxi/admin.py
@@ -3,7 +3,18 @@ from django.contrib.auth.admin import UserAdmin
 from .models import Driver, Car, Manufacturer
 
 
-@admin.register(Driver)
+@admin.register(Manufacturer)  # ✅ Реєструємо Manufacturer через декоратор
+class ManufacturerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Car)  # ✅ Правильна реєстрація Car
+class CarAdmin(admin.ModelAdmin):
+    search_fields = ("model",)
+    list_filter = ("manufacturer",)
+
+
+@admin.register(Driver)  # ✅ Прибираємо повторне `admin.site.register(Driver)`
 class DriverAdmin(UserAdmin):
     list_display = UserAdmin.list_display + ("license_number",)
     fieldsets = UserAdmin.fieldsets + (
@@ -23,12 +34,3 @@ class DriverAdmin(UserAdmin):
             ),
         )
     )
-
-
-@admin.register(Car)
-class CarAdmin(admin.ModelAdmin):
-    search_fields = ("model",)
-    list_filter = ("manufacturer",)
-
-
-admin.site.register(Manufacturer)
--- taxi/views.py
+++ taxi/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
 
 from taxi.models import Driver, Car, Manufacturer
 
-
 def index(request):
     """View function for the home page of the site."""
-
     context = {
         "num_drivers": Driver.objects.count(),
         "num_cars": Car.objects.count(),
@@ -13,3 +12,32 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+
+class ManufacturerListView(generic.ListView):
+    model = Manufacturer
+    queryset = Manufacturer.objects.order_by("name")
+    paginate_by = 5
+
+
+class DriverListView(generic.ListView):
+    model = Driver
+    paginate_by = 5
+    queryset = Driver.objects.order_by("last_name")
+
+
+class DriverDetailView(generic.DetailView):
+    model = Driver
+    queryset = Driver.objects.prefetch_related("cars__manufacturer")
+
+
+class CarListView(generic.ListView):
+    model = Car
+    paginate_by = 5
+    queryset = (Car.objects.select_related("manufacturer")
+                .prefetch_related("drivers")
+                .order_by("model"))
+
+
+class CarDetailView(generic.DetailView):
+    model = Car
